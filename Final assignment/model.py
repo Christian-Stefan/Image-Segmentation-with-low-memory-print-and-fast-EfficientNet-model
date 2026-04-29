92cc609363c31465a616ff91ea090f67601858d6
Christian-Stefan <117277435+Christian-Stefan@users.noreply.github.com>
2026-04-29 13:18:34 +0200

    <Minor><Documentation> of model.py

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -1,15 +1,16 @@
-# Efficient Net infrastructure import
+### --- Start Efficient Net infrastructure imports --- ###
 from efficientnet_pytorch import EfficientNet
 import segmentation_models_pytorch as smp
+### --- End Efficient Net infrastructure imports --- ###
 
-# 1. In order to benefit of the training-predicting workflow as provided
-# wrapping the Model's class initialization in a class is required
+
+# 1. In order to benefit of the train.py as provided
+# wrapping the Model's class initialization in a class is required;
 def get_model():
     return smp.Unet(
-        encoder_name="efficientnet-b3",        # Use your chosen backbone
-        encoder_weights="imagenet",            # Start with pre-trained knowledge
-        in_channels=3,                         # RGB input
+        encoder_name="efficientnet-b3",        # Use an eff.net-b3 as encoder
+        encoder_weights="imagenet",            # Start with pre-trained on imagenet encoder weights
+        in_channels=3,                         
         classes=19                            # 19 Cityscapes evaluation classes
     )
-
 
